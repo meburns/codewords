0dfe16736afa4490ff9fe5edc19789dcaa338b0a
Mike Burns <meburns90@gmail.com>
2020-04-10 01:12:32 -0500

    Add basic game data endpoints

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,9 +1,55 @@
 import axios from "axios";
+const url = "http://localhost:3333";
 
 export function initGame(cb) {
   axios({
+    method: "POST",
+    url: `${url}/games`,
+    headers: { "Content-Type": "application/json; charset=utf-8", Authorization: "blahhhh" },
+    crossDomain: true
+  })
+  .then(res => {
+    cb(res.data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+}
+
+export function getGame(identity, cb) {
+  axios({
     method: "GET",
-    url: `http://localhost:3333/init`,
+    url: `${url}/games/${identity}`,
+    headers: { "Content-Type": "application/json; charset=utf-8", Authorization: "blahhhh" },
+    crossDomain: true
+  })
+  .then(res => {
+    cb(res.data[0].data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+}
+
+export function getGames(cb) {
+  axios({
+    method: "GET",
+    url: `${url}/games`,
+    headers: { "Content-Type": "application/json; charset=utf-8", Authorization: "blahhhh" },
+    crossDomain: true
+  })
+  .then(res => {
+    cb(res.data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+}
+
+export function updateGame(identity, cb) {
+  axios({
+    method: "PUT",
+    url: `${url}/games/${identity}`,
     headers: { "Content-Type": "application/json; charset=utf-8", Authorization: "blahhhh" },
     crossDomain: true
   })
